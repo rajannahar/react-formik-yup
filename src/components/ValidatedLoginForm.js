910c0a4c1040926047c5b16d710f7cdff9097f93
rajannahar <R_N101@msn.com>
2020-01-10 18:47:31 +0000

    added yup validation for email and password with error messages for frontend

--- src/components/ValidatedLoginForm.js
+++ src/components/ValidatedLoginForm.js
@@ -12,6 +12,18 @@ const ValidatedLoginForm = () => {
             console.log("Logging in: ", values)
           }, 500);
         }}
+
+        // Yup - validation
+        validationSchema = {Yup.object().shape({
+          email: Yup.string()
+            .email()
+            .required("Email is required"),
+          password: Yup.string()
+            .required("No password provided")
+            .min(8, "Password is too short, 8 chars minimum")
+            .matches(/(?=.*[0-9])/, "Password should contain a number")
+        })}
+
       >
 
         { props => {
@@ -29,12 +41,20 @@ const ValidatedLoginForm = () => {
             <form autoComplete="off" onSubmit={handleSubmit}>
 
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" value={values.email} placeholder="Enter your email" />
+              <input type="text" name="email" value={values.email} placeholder="Enter your email" onChange={handleChange} onBlur={handleChange} className={errors.email && touched.email && "error"} />
+
+              {errors.email && touched.email && (
+                <div className="input-feedback">{errors.email}</div>
+              )}
 
               <br />
 
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" value={values.password} placeholder="Enter your password" />
+              <input type="password" name="password" value={values.password} placeholder="Enter your password" onChange={handleChange} onBlur={handleBlur} className={errors.password && touched.password && "error"} />
+
+              {errors.password && touched.password && (
+                <div className="input-feedback">{errors.password}</div>
+              )}
 
               <br />
 
